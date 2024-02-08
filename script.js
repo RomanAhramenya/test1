8753f6d78c1a26624908af503ba5384ea5d272c0
Roman <romanahramenya@gmail.com>
2024-02-08 18:06:18 +0300

    description

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const burger = document.querySelector(".burger-menu");
 const nav = document.querySelector(".nav-burger");
 const navClose = document.querySelector(".nav-close");
 const body = document.querySelector("body");
+
 const classNavOpen = "nav-open";
+
 burger.addEventListener("click", () => {
   nav.classList.add(classNavOpen);
   body.style.overflowY = "hidden";
@@ -11,3 +13,28 @@ navClose.addEventListener("click", () => {
   nav.classList.remove(classNavOpen);
   body.style.overflowY = "auto";
 });
+
+const timer = document.querySelector("#timer");
+
+const TIME_LIMIT = 18 * 60 * 60; // 18 hours
+let timePassed = 0;
+let timeLeftValue = TIME_LIMIT;
+
+updateTimeLeft();
+
+const timerInterval = setInterval(() => {
+  timePassed = timePassed += 1;
+  timeLeftValue = TIME_LIMIT - timePassed;
+  updateTimeLeft();
+}, 1000);
+
+function updateTimeLeft() {
+  timer.textContent = formatTime(timeLeftValue);
+}
+
+function formatTime(time) {
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+  return `${hours}:${minutes}:${seconds}`;
+}
